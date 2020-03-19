5026c5d6e0c3f98ac9b80c9f98885a355eb1bc8a
daisymacharia <daisy.macharia@andela.com>
2020-03-19 12:58:29 +0300

    add state hook

--- index.tsx
+++ index.tsx
@@ -8,13 +8,40 @@ const React = {
 
     }
 }
-const App = () => (
-    <div classname="react-2020">
-        <h1> Hello, Person </h1>
-        <input type="text" placeholder="name"></input>
-        <p>Exercitation pariatur adipisicing dolor in eiusmod nisi sunt et labore enim est enim fugiat. Mollit consequat veniam sunt nisi exercitation sit duis adipisicing eiusmod. Est nostrud est eu do culpa et cillum elit in. Irure est pariatur do laborum consequat consequat officia proident eu. Esse ad ea mollit elit reprehenderit ullamco adipisicing aliquip commodo. Tempor ut ea ad mollit labore enim laborum Lorem. </p>
-    </div>
-);
+const states = []
+let stateCursor = 0
+
+const useState = (initialState) => {
+    const FREEZECURSOR = stateCursor
+
+    states[ FREEZECURSOR ] = states[ FREEZECURSOR ] || initialState
+
+    let setState = (newState) => {
+        states[ FREEZECURSOR ] = newState
+        rerender()
+    }
+    stateCursor++
+
+    return [ states[ FREEZECURSOR ], setState ]
+
+}
+
+const App = () => {
+    const [ name, setName ] = useState("person")
+    const [ count, setCount ] = useState(0)
+
+    return (
+        <div classname="react-2020">
+            <h1> Hello, { name } </h1>
+            <input type="text" placeholder="name" value={ name } onchange={ e => setName(e.target.value) }></input>
+            <h2>The count is: { count }</h2>
+            <button onclick={ () => setCount(count + 1) }>+</button>
+            <button onclick={ () => setCount(count - 1) }>-</button>
+
+            <p>Exercitation pariatur adipisicing dolor in eiusmod nisi sunt et labore enim est enim fugiat. Mollit consequat veniam sunt nisi exercitation sit duis adipisicing eiusmod. Est nostrud est eu do culpa et cillum elit in. Irure est pariatur do laborum consequat consequat officia proident eu. Esse ad ea mollit elit reprehenderit ullamco adipisicing aliquip commodo. Tempor ut ea ad mollit labore enim laborum Lorem. </p>
+        </div>
+    )
+}
 
 const render = (reactElement, container) => {
     const actualDomElement = document.createElement(reactElement.tag)
@@ -30,5 +57,11 @@ const render = (reactElement, container) => {
         reactElement.props.children.forEach(child => render(child, actualDomElement))
     }
     container.appendChild(actualDomElement)
+}
+
+const rerender = () => {
+    stateCursor = 0
+    document.querySelector('#app').firstChild.remove()
+    render(< App />, document.querySelector('#app'))
 }
 render(< App />, document.querySelector('#app'))
