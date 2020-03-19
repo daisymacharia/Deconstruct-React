c3e59888f81c87ed5beaae7639f0ee9ee59ec7d1
daisymacharia <daisy.macharia@andela.com>
2020-03-19 13:26:56 +0300

    add concurrent data fetching

--- index.tsx
+++ index.tsx
@@ -1,7 +1,16 @@
 const React = {
     createElement: (tag, props, ...children) => {
         if (typeof tag == "function") {
-            return tag(props)
+            try {
+                return tag(props)
+            } catch ({ promise, key }) {
+                promise.then(data => {
+                    PromiseCache.set(key, data)
+                    rerender()
+                })
+                return { tag: 'h1', props: { children: [ "I am loading" ] } }
+            }
+
         }
         let element = { tag, props: { ...props, children } }
         return element
@@ -26,15 +35,33 @@ const useState = (initialState) => {
 
 }
 
+const PromiseCache = new Map()
+const createResource = (promise, key) => {
+    if (PromiseCache.has(key)) {
+        return PromiseCache.get(key)
+    }
+    throw {
+        promise: promise(), key
+    }
+}
+
 const App = () => {
     const [ name, setName ] = useState("person")
     const [ count, setCount ] = useState(0)
+
+    const dogPhotoUrl = createResource(
+        () => fetch('https://dog.ceo/api/breeds/image/random')
+            .then(r => r.json())
+            .then(payload => payload.message), 'dogPhoto'
+    )
 
     return (
         <div classname="react-2020">
             <h1> Hello, { name } </h1>
             <input type="text" placeholder="name" value={ name } onchange={ e => setName(e.target.value) }></input>
             <h2>The count is: { count }</h2>
+            { dogPhotoUrl }
+            <img src={ dogPhotoUrl }></img>
             <button onclick={ () => setCount(count + 1) }>+</button>
             <button onclick={ () => setCount(count - 1) }>-</button>
 
