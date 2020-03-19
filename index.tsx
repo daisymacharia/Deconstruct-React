6116cbce9e4bf57a03b35e35ece4b8aec7e34cb9
daisymacharia <daisy.macharia@andela.com>
2020-03-19 11:44:46 +0300

    create the renderer function

--- index.tsx
+++ index.tsx
@@ -1,13 +1,34 @@
 const React = {
     createElement: (tag, props, ...children) => {
-        let element = {tag, props: {...props, children}}
-
-        console.log(element);
+        if (typeof tag == "function") {
+            return tag(props)
+        }
+        let element = { tag, props: { ...props, children } }
         return element
-        
+
     }
 }
-const a = <div classname="react-2020">
-    <h1> Hello, Person </h1>
-    <p>lorem ipsum blah blah blah</p>
-</div>
+const App = () => (
+    <div classname="react-2020">
+        <h1> Hello, Person </h1>
+        <input type="text" placeholder="name"></input>
+        <p>Exercitation pariatur adipisicing dolor in eiusmod nisi sunt et labore enim est enim fugiat. Mollit consequat veniam sunt nisi exercitation sit duis adipisicing eiusmod. Est nostrud est eu do culpa et cillum elit in. Irure est pariatur do laborum consequat consequat officia proident eu. Esse ad ea mollit elit reprehenderit ullamco adipisicing aliquip commodo. Tempor ut ea ad mollit labore enim laborum Lorem. </p>
+    </div>
+);
+
+const render = (reactElement, container) => {
+    const actualDomElement = document.createElement(reactElement.tag)
+
+    if ([ "string", "number" ].includes(typeof reactElement)) {
+        container.appendChild(document.createTextNode(String(reactElement)))
+        return
+    }
+    if (reactElement.props) {
+        Object.keys(reactElement.props).filter(p => p !== 'children').forEach(p => actualDomElement[ p ] = reactElement.props[ p ])
+    }
+    if (reactElement.props.children) {
+        reactElement.props.children.forEach(child => render(child, actualDomElement))
+    }
+    container.appendChild(actualDomElement)
+}
+render(< App />, document.querySelector('#app'))
